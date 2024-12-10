1d1f7ff3c6ffadfa84142e66cbecb4002d067087
wakelight23 <rlghks0223@gmail.com>
2024-12-10 12:33:46 +0900

    241210

--- programmers/test.js
+++ programmers/test.js
@@ -1,10 +1,17 @@
-function solution(n) {
-  return +String(n)
-    .split("")
-    .sort((a, b) => b - a)
-    .join("");
+// 두 정수 사이의 합
+function solution(a, b) {
+  var answer = 0;
+  let lowernum = [a, b];
+  lowernum.sort((a, b) => a - b);
+
+  for (lowernum[0]; lowernum[0] <= lowernum[1]; lowernum[0]++) {
+    answer += lowernum[0];
+  }
+
+  return answer;
 }
 
-let n = 118372;
+let a = 3;
+let b = 3;
 
-console.log(solution(n));
+console.log(solution(a, b));
