e288f4d0e55548b17aa9fee34a5edb7f766630db
Wakelight23 <102503132+Wakelight23@users.noreply.github.com>
2024-12-17 09:45:06 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 4.53 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,24 @@
+function solution(left, right) {
+  // left에서 right까지 범위 배열
+  let arr1 = Array(right - left + 1)
+    .fill(left)
+    .map((left, right) => left + right);
+  // 각 수마다 약수 개수 판별
+  let lc = [];
+  let result = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    let count = 0;
+    for (let j = 1; j < arr1[i]; j++) {
+      if (arr1[i] % j === 0) {
+        count++;
+      }
+    }
+    if (count % 2 === 0) {
+      result -= arr1[i];
+    } else {
+      result += arr1[i];
+    }
+  }
+
+  return result;
+}
